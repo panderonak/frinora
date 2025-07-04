58d1affb6042d4cf59995d66f0e43e9c43183da6
panderonak <pandeyronak139@gmail.com>
2025-07-04 21:55:04 +0530

    feat(auth): add Sign In and Sign Up pages using Clerk.

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import { Navbar } from '@/components/navbar';
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,9 +5,10 @@ import { SignOutButton } from '@clerk/nextjs';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 import { Icons } from './icons';
+import { currentUser } from '@clerk/nextjs/server';
 
-export const Navbar = () => {
-  const user = false;
+export const Navbar = async () => {
+  const user = await currentUser();
   return (
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
